f3a145056559ec907ccbd530183654764631879f
chaavisa <aungc342@newschool.edu>
2023-03-03 11:38:10 -0500

    update

---
+++ mar3/ex1.js
@@ -0,0 +1,32 @@
+{
+    name: "Panthella Portable Lamp",
+    designer: "Louis Poulsen",
+    type: "lamp",
+    color: "orange",
+}
+{
+    name: "LC3 Grande Lounge Chair",
+    designer: "Le Corbusier",
+    type: "chair",
+    color: "black",
+}
+{
+    'name': 'Componibili Storage Unit',
+    'designer': 'Anna Castelli Ferrieri',
+    'type': 'storage',
+    'color': 'red',
+}
+{
+    'name': 'Flowerpot Portable Table Lamp',
+    'designer': 'Verner Panton',
+    'type': 'lamp',
+    'color': 'red',
+}
+{
+    'name': 'Etcetera Lounge Chair',
+    'designer': 'Jan Ekselius',
+    'type': 'chair',
+    'color': 'blue',
+}
+
+
