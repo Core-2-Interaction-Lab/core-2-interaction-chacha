471625100c0f30572608a4ab1528e53429703b6f
chaavisa <aungc342@newschool.edu>
2023-05-09 06:12:52 -0400

    update

--- data/data.js
+++ data/data.js
@@ -11,31 +11,23 @@ fetch(url)
 // QUERY, MANIPULATE, VISUALIZE THE DATASET
 const playWithData = data => {
 
-	console.log(data);
-	// how many instances were QUAA-ING?
-	const running = data.filter(item => item.running == true).length;
-	const eating = data.filter(item => item.eating == true).length;
-	const foraging = data.filter(item => item.foraging == true).length;
-	const chasing = data.filter(item => item.chasing == true).length;
-	const climbing = data.filter(item => item.climbing == true).length;
-	
-
-	const dataLabels = ['running', 'eating', 'foraging', 'chasing', 'climbing'];
-	const dataValues = [running, eating, foraging, chasing, climbing];
-
 
 	const ctx = document.getElementById('myChart');
 
 	new Chart(ctx, {
-	  type: 'doughnut',
+	    type: 'pie',
 	  data: {
-		labels: ['running', 'eating', 'foraging', 'chasing', 'climbing'],
+		labels: ['gray', 'cinnamon', 'black', 'no value'],
 		datasets: [
 			{
-				label: '# of Squirrels',
-				data: [running, eating, foraging, chasing, climbing],
-				borderColor: '#4dc9f6',
-				backgroundColor: ['rgba(175, 173, 165, 1)','rgb(150, 150, 250)','rgba(187, 115, 54, 1)','rgba(43, 47, 48, 1)','rgba(150, 116, 81, 1)','rgba(36, 65, 140, 1)','rgba(36, 65, 140, 1)','rgba(36, 65, 140, 1)'],
+				label: 'number of squirrels',
+				data: [2473, 392, 103, 55],
+				backgroundColor: [
+					'rgba(175, 173, 165, 1)',
+					'rgba(187, 115, 54, 1)',
+					'rgba(43, 47, 48, 1)',
+					'rgba(222, 225, 229, 1)',
+				],
 				borderWidth: 0
 			}
 		]
@@ -50,3 +42,4 @@ const playWithData = data => {
 	});
 
 }
+
--- data/data2.js
+++ data/data2.js
@@ -70,3 +70,63 @@ boxButton.addEventListener('click', () => {
     }
     isFood = !isFood;
   });
+
+
+  // HOW TO FETCH DATA ACROSS THE WEB
+const url = 'https://data.cityofnewyork.us/resource/vfnx-vebw.json';
+
+
+fetch(url)
+  .then(response => response.json())
+	// pass the data to the function!
+	.then(data => playWithData(data))
+
+
+// QUERY, MANIPULATE, VISUALIZE THE DATASET
+const playWithData = data => {
+
+	console.log(data);
+	// how many instances were QUAA-ING?
+	const running = data.filter(item => item.running == true).length;
+	const eating = data.filter(item => item.eating == true).length;
+	const foraging = data.filter(item => item.foraging == true).length;
+	const chasing = data.filter(item => item.chasing == true).length;
+	const climbing = data.filter(item => item.climbing == true).length;
+	
+
+	const dataLabels = ['running', 'eating', 'foraging', 'chasing', 'climbing'];
+	const dataValues = [running, eating, foraging, chasing, climbing];
+
+
+	const ctx = document.getElementById('myChart');
+
+	new Chart(ctx, {
+	  type: 'bar',
+	  data: {
+		labels: ['running', 'eating', 'foraging', 'chasing', 'climbing'],
+		datasets: [
+			{
+				label: 'number of squirrels',
+				data: [running, eating, foraging, chasing, climbing],
+				borderColor: '#4dc9f6',
+				backgroundColor: [
+                'rgba(222, 225, 229, 1)',
+                'rgba(175, 173, 165, 1)',
+                'rgba(187, 115, 54, 1)',
+                'rgba(150, 116, 81, 1)',
+                'rgba(43, 47, 48, 1)'
+            ],
+				borderWidth: 0
+			}
+		]
+	  },
+	  options: {
+		scales: {
+		  y: {
+			beginAtZero: true
+		  }
+		}
+	  }
+	});
+
+}
